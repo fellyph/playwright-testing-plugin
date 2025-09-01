c9f862e4afae6262e3f787bb73bb14a837ae822d
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-09-01 17:12:42 +0200

    adding cli demo

---
+++ demos/demo-wp-cli.ts
@@ -0,0 +1,18 @@
+import { runCLI, RunCLIServer } from "@wp-playground/cli";
+
+let cliServer: RunCLIServer;
+
+
+cliServer = await runCLI({
+    command: 'server',
+    wp: 'latest',
+    login: true,
+    blueprint: {
+        steps: [
+            {
+                "step": "wp-cli",
+                "command": `wp post create --post_title='Welcome to Playground' --post_status='published' --post_type='post' --post_content='This is a test post created by the blueprint'`
+            },
+        ],
+    },
+} );
