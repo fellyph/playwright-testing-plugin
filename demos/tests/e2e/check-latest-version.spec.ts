dcf51c3302e6aca8d609c4ad5dd7c711c17bf141
Fellyph Cintra <fellyph.cintra@gmail.com>
2025-09-01 17:34:40 +0200

    updating wordpress version test

--- demos/tests/e2e/check-latest-version.spec.ts
+++ demos/tests/e2e/check-latest-version.spec.ts
@@ -1,7 +1,7 @@
 import { test, expect } from '@playwright/test';
 import { runCLI, RunCLIArgs, RunCLIServer } from "@wp-playground/cli";
 
-test.describe('set-wordpress-language', () => {
+test.describe('check-latest-version', () => {
     let cliServer: RunCLIServer;
 
     test.afterEach(async () => {
@@ -10,28 +10,32 @@ test.describe('set-wordpress-language', () => {
         }
     });
 
-	test('should set WordPress site language to Portuguese (Brazil)', async () => {
-		const expectedLanguage = 'pt_BR';
-		
-		cliServer = await runCLI({
-			command: 'server',
-			wp: 'latest',
-			blueprint: {
-				steps: [
-					{
-						"step": "setSiteLanguage",
-						"language": "pt_BR"
-					},
-				],
-			},
-		} as RunCLIArgs);
-		
-		const response = await cliServer.playground.request({
-			url: '/',
-			method: 'GET',
+	const phpCode = `<?php
+				require_once '/wordpress/wp-load.php';
+				echo get_bloginfo("version");
+			?>`;
+
+	const wordpressVersions = ['6.4', '6.5', '6.6', '6.7', '6.8'];
+
+	for (const wpVersion of wordpressVersions) {
+		test(`should set WordPress site Version to ${wpVersion}`, async () => {
+			cliServer = await runCLI({
+				command: 'server',
+				wp: wpVersion,
+			} as RunCLIArgs);
+
+			await cliServer.playground.writeFile(
+				'/wordpress/version.php',
+				phpCode
+			);
+			
+			const response = await cliServer.playground.request({
+				url: '/version.php',
+				method: 'GET',
+			});
+			
+			expect(response.httpStatusCode).toBe(200);
+			expect(response.text.trim()).toContain(wpVersion);
 		});
-		
-		expect(response.httpStatusCode).toBe(200);
-		expect(response.text.trim()).toBe(expectedLanguage);
-	});
+	}
 });
